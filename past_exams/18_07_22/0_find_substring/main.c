d4ceaa4c6b666a44573da2df1d7fbf500f755fc9
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2023-06-04 16:31:01 +0200

    Update main.c

--- past_exams/18_07_22/0_find_substring/main.c
+++ past_exams/18_07_22/0_find_substring/main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
 
+char* findSubstring(char s1[], char s2[])
+{
+    int l1,l2,i;
+    l1 = strlen(s1);
+    l2 = strlen(s2);
+  
+    for (i=0; i<=l1-l2;i++)
+    {
+        if (strncmp(s1+i,s2,l2)==0)
+            return s1+i;
+    }
+    return NULL;
+}
+
+int main()
+{
+    printf("%s", findSubstring("substring","str"));
+}
