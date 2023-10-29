c03cb763a30da4259dc4e0a04c24cdedc999858e
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2023-10-29 15:37:40 +0100

    Update struct3_pointer.c

--- structs/struct3_pointer.c
+++ structs/struct3_pointer.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+struct str {
+    int x;
+    int y;
+};
+
+int main()
+{
+    struct str a = {0,1};
+    struct str *ptr = &a;
+
+    // two different ways to access //
+    printf("%d %d\n", ptr->x, (*ptr).y);
+
+    return 0;
+}
