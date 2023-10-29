907659c257ca1135538098ba8cef58099c33bc4e
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2023-10-29 13:33:00 +0100

    Update argc_argv.c

--- command_line_arguments/argc_argv.c
+++ command_line_arguments/argc_argv.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+// argc is the number of arguments
+// argv is an array of strings holding each argument
+
+int main (int argc, char * argv[])
+{
+    // print number of arguments passed to our program
+    printf("argc: %d\n", argc);
+    for (int i=0; i<argc; i++)
+        printf("argv[%d] ==> %s\n", i, argv[i]);
+    return 0;
+}
+
 
