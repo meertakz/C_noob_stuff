174ceee3600c04eb886f38be0d6b1fd8fa7a4664
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2023-10-29 15:23:38 +0100

    Update struct2_typedef.c

--- structs/struct2_typedef.c
+++ structs/struct2_typedef.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+typedef struct car
+{
+    char engine[50];
+    char fuel_type[10];
+    int fuel_tank;
+    int seating;
+    float km;
+}car;
+
+int main()
+{
+    car c1 = {"120hp","gas",45,5,12};
+    car c2 = {"90hp","diesel",30,4,8.5};
+    car c3 = {.fuel_tank=70, .seating=6, .engine="150hp",.fuel_type="LPG", .km=15};
+
+    printf("%s\n",c1.engine);
+    printf("%s\n",c2.engine);
+    printf("%s\n",c3.engine);
+    return 0;
+}
