e9ae771d09aa225bcc6d38c4f3e5b18efe381378
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2023-10-31 13:56:02 +0100

    Update 2D_array.c

--- dynamic_memo_allocation/2D_array.c
+++ dynamic_memo_allocation/2D_array.c
@@ -1,1 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+int main()
+{
+    int r, c, i, j;
+    int **mat;
+
+    // row
+    printf("number of rows  #");
+    scanf("%d",&r);
+
+    mat = (int**)malloc(r*sizeof(int*));
+    if (mat == NULL){
+        fprintf(stderr, "memory allocation error\n");
+        exit(1);
+    }
+
+    // col
+    printf("number of cols  #");
+    scanf("%d",&c);
+
+    for (i=0; i<r; i++)
+    {
+        mat[i] = (int*)malloc(c*sizeof(int));
+        if (mat[i]==NULL)
+        {
+            fprintf(stderr, "memory allocation error\n");
+            exit(1);
+        }
+    }
+
+    // matrix manipulation
+    for (i=0;i<r;i++){
+        for (j=0;j<c;j++){
+            printf("mat[%d][%d]:", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    // print matrix
+    for (i=0;i<r;i++){
+        for (j=0;j<c;j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    // free memory
+    for(i=0;i<r;i++){
+        free(mat[i]);
+    }
+    free(mat);
+}
