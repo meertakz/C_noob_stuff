a2ab869a790325a08e7666c7497b7156c42b58df
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2023-11-14 12:29:29 +0100

    Create main5.c

---
+++ recursion/main5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define MAX 10
+#define WANNAFIND 101
+
+int bin_search (int v[], int l, int r, int k){
+    int c;
+    if (l > r){
+        return -1;
+    }
+    c = (l + r) / 2;
+
+    if (k < v[c])
+        return bin_search(v, l, c-1, k);
+    if (k > v[c])
+        return bin_search(v, c+1, r, k);
+    return c;
+}
+
+
+int main(){
+    int v[MAX] = {0};
+
+    for (int i=0; i<MAX; i++){
+        v[i] = i+100;
+        printf("%d ", v[i]);
+    }
+
+    int binary = bin_search(v, 0, 9, WANNAFIND);
+
+    printf("\n%d", binary);
