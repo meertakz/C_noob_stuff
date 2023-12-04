196fbd76e3a314e3d0e950bb29ed3207f4caae5b
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2023-12-04 10:12:23 +0100

    Create main.c

---
+++ simple/lab_5/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define DEBUG 1
+
+void gray (int **, int, int, int);
+int **malloc2d (int, int);
+void printMat (int **, int, int);
+
