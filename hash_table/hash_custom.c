fdbc69d2d2bd97a2a68d167f57c26b742dcf501d
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2023-12-12 18:19:52 +0100

    Update hash_custom.c

--- hash_table/hash_custom.c
+++ hash_table/hash_custom.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* a single node */
 typedef struct item {
     int data;
     int key;
 } item_t;
 
-item_t *newItem(int key, int data) {
-    item_t *result = malloc(sizeof(item_t));
-    result->key = key;
-    result->data = data;
-    return result;
-}
 
-void freeItem(item_t *item) {
-    if (item != NULL)
-        free(item);
-}
-
+/* hash table struct */
 typedef struct hashTable {
     int count;
     int size;
@@ -25,118 +17,23 @@ typedef struct hashTable {
     item_t **array;
 } hashTable_t;
 
-hashTable_t *newHashTable(int size) {
-    hashTable_t *result = malloc(sizeof(hashTable_t));
-    result->count = 0;
-    result->size = size;
-    result->deletedItem = newItem(-1, -1);
-    result->array = malloc(sizeof(item_t *) * size);
-    return result;
-}
 
-void freeHashTable(hashTable_t *table) {
-    if (table != NULL) {
-        for (int i = 0; i < table->size; i++) {
-            item_t *item = table->array[i];
-            if (item != NULL && item != table->deletedItem)
-                free(item);
-        }
-        free(table->array);
-        freeItem(table->deletedItem);
-        free(table);
-    }
-}
-
-int hasItem(hashTable_t *table, int index) {
-    return table->array[index] != NULL && table->array[index] != table->deletedItem;
-}
-
-void display(hashTable_t *table) {
-    for (int i = 0; i < table->size; i++) {
-        if (hasItem(table, i)) {
-            item_t *item = table->array[i];
-            printf("%d) (%d,%d)\n", i, item->key, item->data);
-        } else {
-            printf("%d) empty\n", i);
-        }
-    }
-}
-
-int isFull(hashTable_t *table) {
-    return table->count == table->size;
-}
-
-int hashCode(int key, int size) {
-    return key % size;
-}
-
-int insert(hashTable_t *table, int key, int data) {
-    int result = 0;
-
-    if (isFull(table)) {
-        printf("hash table is full..\n");
-    } else {
-        item_t *item = newItem(key, data);
-        int index = hashCode(key, table->size);
-
-        while (hasItem(table, index))
-            index = hashCode(index + 1, table->size);
-
-        table->array[index] = item;
-        table->count++;
-    }
-
-    return result;
-}
-
-int search(hashTable_t *table, int key) {
-    int result = -1;
-    int index = hashCode(key, table->size);
-    int counter = table->size;
-
-    while (table->array[index] != NULL && counter > 0) {
-        if (table->array[index]->key == key) {
-            result = index;
-            break;
-        }
-        counter--;
-        index = hashCode(index + 1, table->size);
-    }
-
-    return result;
-}
-
-item_t *h_remove(hashTable_t *table, int key) {
-    item_t *result = NULL;
-    int index = search(table, key);
-
-    if (index != -1) {
-        result = table->array[index];
-        table->array[index] = table->deletedItem;
-        table->count--;
-    }
-    return result;
-}
-
-int input() {
-    int number;
-    char buffer[100];
-    while (1) {
-        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-            exit(1);
-        }
-        if (sscanf(buffer, "%d", &number) == 1) {
-            break;
-        } else {
-            printf("==================\n");
-            printf("Invalid input !!!\n#");
-
-        }
-    }
-    return number;
-}
+/* function declarations */
+item_t *newItem(int, int);
+void freeItem(item_t *);
+hashTable_t *newHashTable(int);
+void freeHashTable(hashTable_t *);
+int hasItem(hashTable_t *, int);
+void display(hashTable_t *);
+int isFull(hashTable_t *);
+int hashCode(int, int);
+int insert(hashTable_t *, int, int);
+int search(hashTable_t *, int);
+item_t *h_remove(hashTable_t *, int);
+int input(void);
 
 
+/* main program */
 int main() {
     int itemIndex = -1;
     item_t *item = NULL;
@@ -226,4 +123,138 @@ int main() {
             }
         }
     }
+}
+
+
+
+item_t *newItem(int key, int data) {
+    item_t *result = malloc(sizeof(item_t));
+    result->key = key;
+    result->data = data;
+    return result;
+}
+
+
+void freeItem(item_t *item) {
+    if (item != NULL)
+        free(item);
+}
+
+
+hashTable_t *newHashTable(int size) {
+    hashTable_t *result = malloc(sizeof(hashTable_t));
+    result->count = 0;
+    result->size = size;
+    result->deletedItem = newItem(-1, -1);
+    result->array = malloc(sizeof(item_t *) * size);
+    return result;
+}
+
+
+void freeHashTable(hashTable_t *table) {
+    if (table != NULL) {
+        for (int i = 0; i < table->size; i++) {
+            item_t *item = table->array[i];
+            if (item != NULL && item != table->deletedItem)
+                free(item);
+        }
+        free(table->array);
+        freeItem(table->deletedItem);
+        free(table);
+    }
+}
+
+
+int hasItem(hashTable_t *table, int index) {
+    return table->array[index] != NULL && table->array[index] != table->deletedItem;
+}
+
+
+void display(hashTable_t *table) {
+    for (int i = 0; i < table->size; i++) {
+        if (hasItem(table, i)) {
+            item_t *item = table->array[i];
+            printf("%d) (%d,%d)\n", i, item->key, item->data);
+        } else {
+            printf("%d) empty\n", i);
+        }
+    }
+}
+
+
+int isFull(hashTable_t *table) {
+    return table->count == table->size;
+}
+
+
+int hashCode(int key, int size) {
+    return key % size;
+}
+
+
+int insert(hashTable_t *table, int key, int data) {
+    int result = 0;
+    if (isFull(table)) {
+        printf("hash table is full..\n");
+    } else {
+        item_t *item = newItem(key, data);
+        int index = hashCode(key, table->size);
+
+        while (hasItem(table, index))
+            index = hashCode(index + 1, table->size);
+        table->array[index] = item;
+        table->count++;
+    }
+
+    return result;
+}
+
+
+int search(hashTable_t *table, int key) {
+    int result = -1;
+    int index = hashCode(key, table->size);
+    int counter = table->size;
+
+    while (table->array[index] != NULL && counter > 0) {
+        if (table->array[index]->key == key) {
+            result = index;
+            break;
+        }
+        counter--;
+        index = hashCode(index + 1, table->size);
+    }
+
+    return result;
+}
+
+
+item_t *h_remove(hashTable_t *table, int key) {
+    item_t *result = NULL;
+    int index = search(table, key);
+
+    if (index != -1) {
+        result = table->array[index];
+        table->array[index] = table->deletedItem;
+        table->count--;
+    }
+    return result;
+}
+
+
+int input(void) {
+    int number;
+    char buffer[100];
+    while (1) {
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            exit(1);
+        }
+        if (sscanf(buffer, "%d", &number) == 1) {
+            break;
+        } else {
+            printf("==================\n");
+            printf("Invalid input !!!\n#");
+
+        }
+    }
+    return number;
 }
